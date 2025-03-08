09b4c7af0a6a6de535c885c80c6ba61bfd2f262b
Mikaell Miguel da Silva <mikaell.miguel.br@gmail.com>
2025-03-08 19:01:34 -0300

    feat(frontend): criando o global style

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Homepage } from './pages/Homepage'
+import { GlobalStyles } from './styles/global'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <GlobalStyles />
     <Homepage />
   </StrictMode>,
 )
