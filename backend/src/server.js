e77dae4dabb28e439b362a5040d5acd47253479c
Mikaell Miguel da Silva <mikaell.miguel.br@gmail.com>
2025-03-08 16:13:58 -0300

    feat(database): adicionado a criação de tabelas do bd

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,19 @@
 const express = require('express');
 require('dotenv').config();
-
+const knex = require('./database/knex');
 const routes = require('./routes');
+
+// Criando as tabelas no Banco de Dados
+knex.migrate.latest()
+  .then(() => {
+    console.log('Migrations executadas com sucesso!');
+  })
+  .catch(error => {
+    console.error('Erro ao executar as migrations:', error);
+  })
+  .finally(() => {
+    knex.destroy();
+  });
 
 const app = express();
 app.use(express.json());
