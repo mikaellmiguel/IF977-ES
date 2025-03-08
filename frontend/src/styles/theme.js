1b38dbc83ec9d6b1ca433be8711947cfa8825fbb
Mikaell Miguel da Silva <mikaell.miguel.br@gmail.com>
2025-03-08 18:52:00 -0300

    feat(frontend): adicionando a paleta de cores

---
+++ frontend/src/styles/theme.js
@@ -0,0 +1,25 @@
+export default {
+    COLORS: {
+      EMERALD_50: '#ecfdf5',
+      EMERALD_100: '#d1fae5',
+      EMERALD_200: '#a7f3d0',
+      EMERALD_300: '#6ee7b7',
+      EMERALD_400: '#34d399',
+      EMERALD_500: '#10b981',
+      EMERALD_600: '#059669',
+      EMERALD_700: '#047857',
+      EMERALD_800: '#065f46',
+      EMERALD_900: '#064e3b',
+  
+      SAGE_50: '#f8faf8',
+      SAGE_100: '#e8efe8',
+      SAGE_200: '#d1e0d1',
+      SAGE_300: '#a8c3a8',
+      SAGE_400: '#7fa17f',
+      SAGE_500: '#5f835f',
+      SAGE_600: '#4a684a',
+      SAGE_700: '#3d543d',
+      SAGE_800: '#334433',
+      SAGE_900: '#2b382b',
+    }
+  }
