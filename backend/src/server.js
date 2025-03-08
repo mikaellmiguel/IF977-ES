cdbea2f796b7173f518abfde14646b6836c49221
Mikaell Miguel da Silva <mikaell.miguel.br@gmail.com>
2025-03-08 16:36:40 -0300

    feat: adicionando tratativa de erros no express

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,6 +10,23 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
+app.use((error, request, response, next) => {
+    
+  if(error instanceof AppError) {
+      return response.status(error.statusCode).json({
+          status: "error",
+          message: error.message
+      })
+  }
+
+  console.error(error)
+
+  return response.status(500).json({
+      status: "error",
+      message: "Internal Server Error"
+  })
+})
+
 const SERVER_PORT = process.env.SERVER_PORT || 5000;
 
 app.listen(SERVER_PORT, () => {
