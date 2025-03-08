a2e8b76bf806f81c6c105ae47fa2e79f714afe25
Mikaell Miguel da Silva <mikaell.miguel.br@gmail.com>
2025-03-08 17:11:39 -0300

    feat: criação do controller de sessões

---
+++ backend/src/controllers/SessionsController.js
@@ -0,0 +1,12 @@
+const knex = require("../database/knex");
+const AppError = require("../utils/AppError");
+const {compare} = require("bcryptjs");
+const authConfig = require("../configs/auth")
+const {sign} = require("jsonwebtoken");
+
+
+class SessionsController {
+   
+}
+
+module.exports = SessionsController;
