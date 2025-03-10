49b6de60f4bc10878f828192032c2bbf063f7877
Mikaell Miguel da Silva <mikaell.miguel.br@gmail.com>
2025-03-09 23:35:48 -0300

    feat(frontend): adicionando navegação na landing page da aplicação

--- frontend/src/pages/LandingPage/index.jsx
+++ frontend/src/pages/LandingPage/index.jsx
@@ -1,8 +1,19 @@
 import { Button } from "../../components/Button";
 import { Container, Content, Header, Img, Logo, Options, TextContent } from "./styles";
+import { useNavigate } from "react-router-dom";
 
 
 export function LandingPage() {
+  
+  const navigate = useNavigate();
+
+  function handleNavigateToSignIn() {
+    navigate("/login");
+  }
+
+  function handleNavigateToSignUp() { 
+    navigate("/register");
+  }
   
   return (
     <Container>
@@ -12,8 +23,8 @@ export function LandingPage() {
           <h1>GeoMundo</h1>
         </Logo>
         <Options>
-          <Button>Entrar</Button>
-          <Button>Cadastre-se</Button>
+          <Button onClick={handleNavigateToSignIn}>Entrar</Button>
+          <Button onClick={handleNavigateToSignUp}>Cadastre-se</Button>
         </Options>
        
       </Header>
@@ -23,7 +34,7 @@ export function LandingPage() {
           <h1>GeoMundo: Conheça os Países, <br/> Explore o Planeta.</h1>
           <p>O GeoMundo é sua plataforma de referência para explorar o mundo.</p>
           <p>Descubra informações detalhadas sobre países, incluindo dados geográficos, descrições rápidas e mapas interativos. Ideal para estudantes, viajantes e curiosos por conhecimento.</p>
-          <Button>Começe a sua Jornada!</Button>
+          <Button onClick={handleNavigateToSignUp}>Começe a sua Jornada!</Button>
         </TextContent>
         <Img />
       </Content>
