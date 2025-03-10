fb30efde3dcf2481ac6f70bef76c8a530dc08744
Mikaell Miguel da Silva <mikaell.miguel.br@gmail.com>
2025-03-10 00:12:57 -0300

    chore(frontend): instalação e configuração do toastify

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import { GlobalStyles } from './styles/global'
 import { AuthProvider } from './hooks/auth'
 import { Routes } from './routes'
+import { ToastContainer } from 'react-toastify'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <GlobalStyles />
+    <ToastContainer position='top-center'/>
     <AuthProvider>
         <Routes />
     </AuthProvider>
