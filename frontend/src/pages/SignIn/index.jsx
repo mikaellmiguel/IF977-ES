8d7916c4c696f3288da202d18cb1c9cab43f85b5
Mikaell Miguel da Silva <mikaell.miguel.br@gmail.com>
2025-03-09 23:41:14 -0300

    feat(frontend): adicionando navegação/autenticação na tela de login

--- frontend/src/pages/SignIn/index.jsx
+++ frontend/src/pages/SignIn/index.jsx
@@ -1,8 +1,20 @@
 import { Button } from "../../components/Button";
 import { Input } from "../../components/Input";
 import { Container, Form, Left, Img, Buttons} from "./styles";
+import { useState } from "react";
+import { useAuth } from "../../hooks/auth";
 
 export function SignIn() {
+    
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const {signIn} = useAuth();
+
+    function handleSignIn(){
+        signIn({email, password})
+    }
+    
 
     return (
         <Container>
@@ -10,11 +22,11 @@ export function SignIn() {
                 <img src="src/assets/logo.svg" alt="Logo"/>
                 <h1>Bem-Vindo ao Geomundo!</h1>
                 <Form>
-                    <Input type="email" name="email" label="E-mail"/>
-                    <Input type="password" name="password" label="Senha"/>
+                    <Input type="email" name="email" label="E-mail" value={email} onChange={(event) => setEmail(event.target.value)}/>
+                    <Input type="password" name="password" label="Senha" value={password} onChange={(event) => setPassword(event.target.value)}/>
                 </Form>
                 <Buttons>
-                        <Button>Entrar</Button>
+                        <Button onClick={() => handleSignIn()}>Entrar</Button>
                         <Button>Cadastrar-se</Button>
                 </Buttons>
             </Left>
