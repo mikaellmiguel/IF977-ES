187a2526a37691bf4f5b85baab3f3c8f55cdfa5d
Mikaell Miguel da Silva <mikaell.miguel.br@gmail.com>
2025-03-09 22:45:05 -0300

    style(frontend): ajuste no estilo global da aplicação

--- frontend/src/styles/global.js
+++ frontend/src/styles/global.js
@@ -6,10 +6,17 @@ export const GlobalStyles = createGlobalStyle`
         margin: 0;
         padding: 0;
         box-sizing: border-box;
+        font-family:"Inter", system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+    }
+
+    :root {
+        @media (min-width: 1600px) {
+            font-size: 115%;
+        }
     }
 
     body {
         background: ${theme.COLORS.BG_100};
         overflow: hidden;
     }
-`;	
+`;
