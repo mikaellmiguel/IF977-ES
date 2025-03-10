20af336eea8c19504732131c04650ec6d55544a3
Mikaell Miguel da Silva <mikaell.miguel.br@gmail.com>
2025-03-09 22:26:35 -0300

    feat(frontend/AuthContext): adicionando a função de login no contexto de autenticação

--- frontend/src/hooks/auth.jsx
+++ frontend/src/hooks/auth.jsx
@@ -1,4 +1,42 @@
-import { createContext} from "react";
+/* eslint-disable react-refresh/only-export-components */
+import { createContext, useState } from "react";
+import { api } from "../services/api";
+
 
 
 export const AuthContext = createContext({})
+
+function AuthProvider({children}) {
+    
+    const [data, setData] = useState({});
+    
+    async function signIn({email, password}) {
+        try{
+            const response = await api.post("/sessions", {email, password});
+            
+            const {token, user} = response.data;
+
+            localStorage.setItem("@Geomundo:token", token);
+            localStorage.setItem("@Geomundo:user", JSON.stringify(user));
+
+            api.defaults.headers.authorization = `Bearer ${token}`;
+            setData({user, token});
+        }
+        catch(error){
+            if(error.response) {
+                alert(error.response.data.message);
+            }
+            else{
+                alert("Não foi possível realizar o login");
+            }
+        }
+    }
+
+    return(
+        <AuthContext.Provider value={{
+            signIn,
+            user:data.user}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
