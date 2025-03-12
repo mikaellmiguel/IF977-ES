b16da9c9f1811acf9cf1ef10ab21f832f1897f3c
Mikaell Miguel da Silva <mikaell.miguel.br@gmail.com>
2025-03-12 07:43:54 -0300

    feat(backend/CountriesController): Add função que retorna um país com o CCN3 informado

--- backend/src/controllers/CountriesController.js
+++ backend/src/controllers/CountriesController.js
@@ -1,10 +1,28 @@
 const getDataCountries = require('../utils/getDataCountries');
+const AppError = require('../utils/AppError');
 
 class CountriesController {
 
     async index(request, response) {
         const countries = getDataCountries();
         return response.json(countries);
+    }
+
+    async show(request, response) {
+        const countries = getDataCountries();
+        const { ccn3 } = request.params;
+
+        if (!ccn3) {
+            throw new AppError("Informe o ID do país");
+        }
+        
+        const country = countries.find(country => country.ccn3 == ccn3);
+
+        if (!country) {
+            throw new AppError("País não encontrado");
+        }
+
+        return response.json(country);
     }
 }
 
