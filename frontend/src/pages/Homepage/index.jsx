7f671b06d506f7296589861eafdf65855c2796c8
Mikaell Miguel da Silva <mikaell.miguel.br@gmail.com>
2025-03-12 08:31:12 -0300

    feat(frontend/pages): crianção da Homepage

--- frontend/src/pages/Homepage/index.jsx
+++ frontend/src/pages/Homepage/index.jsx
@@ -1,8 +1,45 @@
+import { CountryCard } from "../../components/CountryCard";
+import { SearchBar } from "../../components/SearchBar";
+import { SideBar } from "../../components/SideBar";
+import { Container, Content, CountryList } from "./styles";
+import { api } from "../../services/api";
+import { useState } from "react";
+import { useEffect } from "react";
+
 export function Homepage() {
-  return (
-    <div>
-      <h1>Geomundo</h1>
-      <p>Sejam bem vindo, ao geomundo!</p>
-    </div>
-  );
+
+    
+    const [countries, setCountries] = useState([]);
+    const [search, setSearch] = useState("");
+
+    useEffect( () => {
+        async function listCountries(){
+            if(search===""){
+                const response = await api.get("/countries");
+                setCountries(response.data);
+            }
+            else {
+                const response = await api.get(`/countries/name/${search}`);
+                setCountries(response.data);
+            }
+        }
+        listCountries();
+    }, [search]);
+
+    return (
+        <Container>
+            <SideBar></SideBar>
+            <Content>
+                <SearchBar placeholder="Pesquisar" value={search} onChange={(event) => setSearch(event.target.value)}/>
+                <CountryList>
+                    {countries && countries.map((country, index) => (
+
+                        <CountryCard key={index} name={country.name} flag={country.flag}/>
+
+                    ))}
+                </CountryList>
+            </Content>
+        </Container>
+    )
+
 }
