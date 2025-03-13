ff81e6b5e1cb3d165e54439a6ba2b63d282eab5d
Mikaell Miguel da Silva <mikaell.miguel.br@gmail.com>
2025-03-12 23:31:19 -0300

    feat(backend): criando o controller para favoritos

---
+++ backend/src/controllers/FavoritesController.js
@@ -0,0 +1,9 @@
+const knex = require("../database/knex");
+const AppError = require("../utils/AppError");
+const getDataCountries = require("../utils/getDataCountries");
+
+class FavoritesController {
+
+}
+
+module.exports = FavoritesController;
