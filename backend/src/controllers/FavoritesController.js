d3e6550ac19c1956fd1f3ccaf5785f24bb9a42c1
Mikaell Miguel da Silva <mikaell.miguel.br@gmail.com>
2025-03-12 23:32:20 -0300

    feat(backend/FavoritesController): adicionando a função para criação de favorito

--- backend/src/controllers/FavoritesController.js
+++ backend/src/controllers/FavoritesController.js
@@ -3,7 +3,44 @@ const AppError = require("../utils/AppError");
 const getDataCountries = require("../utils/getDataCountries");
 
 class FavoritesController {
+    async create(request, response) {
+        const {id: user_id} = request.user;
+        const {ccn3} = request.body;
 
+        if(!ccn3) {
+            throw new AppError("Informe o CCN3 do país");
+        }
+
+        if(ccn3.length !== 3) {
+            throw new AppError("O CCN3 deve ter 3 dígitos");
+        }
+
+        const checkInFavorites = await knex("favorites").where({user_id, ccn3}).first();
+        
+        if(checkInFavorites) {
+            throw new AppError("País já favoritado");
+        }
+
+        const countries = getDataCountries();
+        const country = countries.find(country => country.ccn3 == ccn3);
+
+        if(!country) {
+            throw new AppError("País não encontrado");
+        }
+
+        await knex("favorites").insert({
+            user_id,
+            country_name: country.name,
+            area: country.area,
+            population: country.population,
+            ccn3,
+            capital: country.capital,
+            flag: country.flag,
+        });
+
+        return response.status(201).json();
+
+    }
 }
 
 module.exports = FavoritesController;
