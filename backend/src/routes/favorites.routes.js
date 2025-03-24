b5c2dc7efcc3e0db3fc35e7430fbaa9775b8fae7
Mikaell Miguel da Silva <mikaell.miguel.br@gmail.com>
2025-03-24 08:33:13 -0300

    fix(backend/routes): ajustando as rotas de favoritos

--- backend/src/routes/favorites.routes.js
+++ backend/src/routes/favorites.routes.js
@@ -7,9 +7,10 @@ const favoritesController = new FavoritesController();
 const favoritesRouter = Router();
 favoritesRouter.use(ensureAuthenticated);
 
-favoritesRouter.post('/', favoritesController.create);
+favoritesRouter.get('/name/:name', favoritesController.searchByName);
+favoritesRouter.post('/:ccn3', favoritesController.create);
 favoritesRouter.get('/', favoritesController.index);
-favoritesRouter.get('/:name', favoritesController.searchByName);
+favoritesRouter.get('/:ccn3', favoritesController.show);
 favoritesRouter.delete('/:ccn3', favoritesController.delete);
 
 module.exports = favoritesRouter;
