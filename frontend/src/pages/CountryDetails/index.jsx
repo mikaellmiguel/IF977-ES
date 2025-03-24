0a4625e5695e2b67c0ba80cc95b16e40a00cf0c2
Mikaell Miguel da Silva <mikaell.miguel.br@gmail.com>
2025-03-24 08:36:25 -0300

    feat(frontend/CountryDetails): adicionando interatividade do botão adicionar favoritos

--- frontend/src/pages/CountryDetails/index.jsx
+++ frontend/src/pages/CountryDetails/index.jsx
@@ -15,6 +15,7 @@ export function CountryDetails() {
   const navigate = useNavigate();
   const {ccn3} = useParams();
   const [country, setCountry] = useState({});
+  const [checkFavorite, setCheckFavorite] = useState(false);
   
   useEffect(() => {
     async function getCountry() {
@@ -25,25 +26,44 @@ export function CountryDetails() {
     getCountry();
   }, [ccn3]);
 
+  useEffect(() => {
+    async function checkFavoriteCountry() {
+      try {
+        await api.get(`/favorites/${ccn3}`);
+        setCheckFavorite(true);
+      } catch {
+        setCheckFavorite(false);
+      }
+    }
+    checkFavoriteCountry();
+  }, [ccn3, checkFavorite]);
+
+  async function handleFavorites() {
+    if(checkFavorite) {
+      await api.delete(`/favorites/${ccn3}`);
+    } else {
+      await api.post(`/favorites/${ccn3}`);
+    }
+    setCheckFavorite(!checkFavorite);
+  }
   
   return (
     <Container>
       {/* Sidebar */}
       <Sidebar>
-        <BackButton onClick={()=> navigate(-1)}>← Voltar</BackButton>
+        <BackButton onClick={() => navigate(-1)}>← Voltar</BackButton>
         <img src={country.flag} alt={`Bandeira do ${country.name}`} />
         <CountryName>{country.name || "País"}</CountryName>
         <InfoList>
-          <InfoItem><TiWorld/> {country.continents || "Continente"}</InfoItem>
-          <InfoItem><IoLocationSharp/>{country.capital || "Capital"}</InfoItem>
-          <InfoItem><IoPerson/> { country.population || "Nº População"}</InfoItem>
-          <InfoItem><MdAttachMoney/> {(country.currency && Object.values(country.currency)[0]?.name) || "Moeda"} ({ (country.currency && Object.values(country.currency)[0]?.symbol) || "Símbolo"})</InfoItem>
-          <InfoItem><FaClock/> { country.timezones ||"Fuso horário Principal"}</InfoItem>
+          <InfoItem><TiWorld /> {country.continents || "Continente"}</InfoItem>
+          <InfoItem><IoLocationSharp /> {country.capital || "Capital"}</InfoItem>
+          <InfoItem><IoPerson /> {country.population || "Nº População"}</InfoItem>
+          <InfoItem><MdAttachMoney /> {(country.currency && Object.values(country.currency)[0]?.name) || "Moeda"} ({(country.currency && Object.values(country.currency)[0]?.symbol) || "Símbolo"})</InfoItem>
+          <InfoItem><FaClock /> {country.timezones || "Fuso horário Principal"}</InfoItem>
         </InfoList>
-        <Button>★ Remover dos Favoritos</Button>
+        <Button onClick={() => handleFavorites()}>{checkFavorite ? "★ Remover dos Favoritos" : "☆ Adicionar aos Favoritos"}</Button>
       </Sidebar>
 
-      {/* Main Content */}
       <MainContent>
         <SectionTitle>Descrição Geral</SectionTitle>
         <Description>
@@ -58,18 +78,20 @@ export function CountryDetails() {
             <SectionTitle>Línguas</SectionTitle>
             <ul>
               {country.languages && Object.values(country.languages).map((language, index) => (
-
                 <li key={index}>{language}</li>
-
               ))}
             </ul>
           </GridItem>
           <GridItem>
             <SectionTitle>Fronteiras</SectionTitle>
             <ul>
-              <li>Em Desenvolvimento</li>
-              <li>Em Desenvolvimento</li>
-              <li>Em Desenvolvimento</li>
+              {country.borders && country.borders.length > 0 ? (
+                country.borders.map((border, index) => (
+                  <li key={index}>{border}</li>
+                ))
+              ) : (
+                <li>Sem fronteiras</li>
+              )}
             </ul>
           </GridItem>
           <GridItem>
