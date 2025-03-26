7bb8ba41d46e5062c9b03871d2e7be7a5d2e1305
Mikaell Miguel da Silva <mikaell.miguel.br@gmail.com>
2025-03-25 22:50:53 -0300

    feat(frontend/routes): adicionando a rota para a página de profile

--- frontend/src/routes/app.routes.jsx
+++ frontend/src/routes/app.routes.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import {Homepage} from '../pages/Homepage';
 import {CountryDetails} from '../pages/CountryDetails';
 import {Favorites} from '../pages/Favorites';
+import { Profile } from '../pages/Profile';
 
 export function AppRoutes() {
     return(
@@ -10,6 +11,7 @@ export function AppRoutes() {
             <Route path="/login" element={<Navigate to="/"/>}/>
             <Route path="/country/:ccn3" element={<CountryDetails/>}/>
             <Route path="/favorites" element={<Favorites/>}/>
+            <Route path="/profile" element={<Profile/>}/>
         </Routes>
     )
 }
